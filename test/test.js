833360f7ae019b1c32ffbe3b03c681c6dc4726cf
vingorius <vingorius@gmail.com>
2016-02-23 10:11:14 +0900

    first commit

---
+++ test/test.js
@@ -0,0 +1,25 @@
+var jadeBeautifier = require('../index');
+var assert = require('assert');
+var fs = require('fs');
+var ENC = 'utf8';
+var before_file_name = './test/before/test.jade';
+
+describe('jadeBeautifier', function() {
+    it('should equal when default option', function(done) {
+        var option = {
+            fill_tab: true,
+            omit_div: false,
+            tab_size: 4
+        };
+        var after_file_name = './test/after/test_true_false_4.jade';
+
+        fs.readFile(before_file_name, ENC, function(err, before) {
+            if (err) throw err;
+            fs.readFile(after_file_name, ENC, function(err, after) {
+                if (err) throw err;
+                assert.equal(after, jadeBeautifier(before, option));
+                done();
+            });
+        });
+    });
+});
