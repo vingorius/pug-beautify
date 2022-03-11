b229fbee1009fbf78a43b89defa45144aad33898
johnsoncodehk <johnsoncodehk@gmail.com>
2022-03-12 05:17:11 +0800

    fix: var -> const, let
    https://github.com/johnsoncodehk/volar/issues/1002

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /**
  * Beautify Pug(former jade) file.
  *
- * var pugBeautify = require('pug-beautify');
- * var beautifiedString = pugBeautify-beautify(code,{fill_tab:true,omit_div:false,tab_size:4});
+ * const pugBeautify = require('pug-beautify');
+ * const beautifiedString = pugBeautify-beautify(code,{fill_tab:true,omit_div:false,tab_size:4});
  *
  * @param  {string} code - strings to be beautified.
  * @param  {Object} opt - options.
@@ -21,19 +21,19 @@ module.exports = function(code, opt) {
   if (typeof code !== 'string') quit('Code must be a string text.');
   if (typeof opt !== 'undefined' && typeof opt !== 'object') quit('Option must be a object.');
 
-  var fill_tab = (typeof opt.fill_tab !== 'undefined') ? opt.fill_tab : true;
-  var omit_div = (typeof opt.omit_div !== 'undefined') ? opt.omit_div : false;
-  var tab_size = (typeof opt.tab_size !== 'undefined') ? opt.tab_size : 4;
-  var separator = opt.separator_space ? ' ' : ', ';
-  var empty_lines = (typeof opt.omit_empty_lines !== 'undefined') ? opt.omit_empty_lines : true;
-  var debug = (typeof opt.debug !== 'undefined') ? opt.debug : false;
+  const fill_tab = (typeof opt.fill_tab !== 'undefined') ? opt.fill_tab : true;
+  const omit_div = (typeof opt.omit_div !== 'undefined') ? opt.omit_div : false;
+  const tab_size = (typeof opt.tab_size !== 'undefined') ? opt.tab_size : 4;
+  const separator = opt.separator_space ? ' ' : ', ';
+  const empty_lines = (typeof opt.omit_empty_lines !== 'undefined') ? opt.omit_empty_lines : true;
+  const debug = (typeof opt.debug !== 'undefined') ? opt.debug : false;
 
   if (debug) console.log(fill_tab, omit_div, tab_size, debug, separator);
 
   // list of indents
-  var indentList = [];
+  const indentList = [];
 
-  var prevIndent = {
+  let prevIndent = {
     type: 'code', // type 'code' or 'remark'
     indent: 0, // count of indent space. it replace tab with space
     tab: 0, // count of tab ,line indent will be filled up with this value.
@@ -44,19 +44,19 @@ module.exports = function(code, opt) {
     code = code.replace(/^\s*[\r\n]/gm, '');
   }
 
-  var lines = code.split('\n');
+  const lines = code.split('\n');
 
   lines.forEach(function(line, n) { // array.forEach is blocking, no async function.
     // it return matching space --> data[0], data[index] = 0, remained input --> data.input
-    var data = line.match(/^\s*/);
+    const data = line.match(/^\s*/);
 
     // when tab and space mixed, it replace all tab to spaces.
-    var tmp = data[0].replace(/\t/g, Array(tab_size + 1).join(' '));
-    var remainedInput = data.input.replace(/^\s*/, '');
-    var indent = (remainedInput.length === 0) ? 0 : tmp.length;
+    const tmp = data[0].replace(/\t/g, Array(tab_size + 1).join(' '));
+    let remainedInput = data.input.replace(/^\s*/, '');
+    const indent = (remainedInput.length === 0) ? 0 : tmp.length;
 
-    var tab = 0;
-    var type = (remainedInput.match(/^\/\/|^\/\*|^\*/)) ? 'remark' : 'code';
+    let tab = 0;
+    const type = (remainedInput.match(/^\/\/|^\/\*|^\*/)) ? 'remark' : 'code';
 
     if (omit_div) {
       remainedInput = remainedInput.replace(/^div(\.|#)/i, '$1');
@@ -74,7 +74,7 @@ module.exports = function(code, opt) {
         } else if (indent > prevIndent.indent) { // when indented, add tab
           tab = prevIndent.tab + 1;
         } else { // when new indent, if find the same indent, and follow it's tab.
-          for (var i = indentList.length - 1; i >= 0; i--) {
+          for (let i = indentList.length - 1; i >= 0; i--) {
             if (indent == indentList[i].indent) {
               tab = indentList[i].tab;
               break;
@@ -93,7 +93,7 @@ module.exports = function(code, opt) {
       }
     }
 
-    var curIndent = {
+    const curIndent = {
       type: type,
       indent: indent,
       tab: tab,
@@ -108,8 +108,8 @@ module.exports = function(code, opt) {
   });
 
   // // Here,it reformat with it's tab count.
-  var formatedLine = indentList.map(function(line, n) {
-    var space = Array(line.tab + 1).join('\t');
+  const formatedLine = indentList.map(function(line, n) {
+    let space = Array(line.tab + 1).join('\t');
     //when fill with space
     if (!fill_tab) space = space.replace(/\t/g, Array(tab_size + 1).join(' '));
 
